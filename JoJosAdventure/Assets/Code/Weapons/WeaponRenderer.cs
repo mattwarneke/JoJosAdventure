34ab9d984b7bff74344bf0aa1b23383b2b058efb
Matt Warneke <warneke.matt@gmail.com>
2023-04-05 22:09:06 -0700

    Finish tail weapon cleanup

--- JoJosAdventure/Assets/Code/Weapons/WeaponRenderer.cs
+++ JoJosAdventure/Assets/Code/Weapons/WeaponRenderer.cs
@@ -5,34 +5,19 @@ namespace JoJosAdventure.Weapons
     [RequireComponent(typeof(SpriteRenderer))]
     public class WeaponRenderer : MonoBehaviour
     {
-        [SerializeField]
-        protected int playerSortingOrder = 0;
-
-        protected SpriteRenderer weaponRenderer;
-
-        private void Awake()
-        {
-            this.weaponRenderer = this.GetComponent<SpriteRenderer>();
-        }
-
         private bool _val = false;
 
         public void FlipSprite(bool val)
         {
-            //this.weaponRenderer.flipY = val;
+            int flipModifier = val ? -1 : 1;
             if (this._val != val)
             {
-                this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y * -1);
+                this.transform.localScale = new Vector3(
+                    this.transform.localScale.x * flipModifier,
+                    Mathf.Abs(this.transform.localScale.y) * flipModifier,
+                    this.transform.localScale.z);
                 this._val = val;
             }
-        }
-
-        public void RenderBehindHead(bool val)
-        {
-            if (val) // set the weapon behind the player
-                this.weaponRenderer.sortingOrder = this.playerSortingOrder - 1;
-            else
-                this.weaponRenderer.sortingOrder = this.playerSortingOrder + 1;
         }
     }
 }
